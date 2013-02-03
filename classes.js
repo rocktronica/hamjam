2fe6d28033392aafd6b0228bf2a54e42c4241abc
rocktronica <tommy@iamnotagoodartist.com>
2013-02-02 22:02:29 -0800

    Indentation?!

--- classes.js
+++ classes.js
@@ -1,100 +1,100 @@
 // Rule
 
 var Rule = function(input){
-	this.value = input;
-	var parts = input.split(/@/);
-	this.active = false;
-	if (parts.length === 2) {
-		this.local = parts[0];
-		this.domain = parts[1];
-		this.active = true;
-		this.url = this.domain.replace(/\*\./g, "").replace(/\*/g, "");
-		this.favicon = "http://www.google.com/s2/favicons?domain=" + this.url;
-	}
+    this.value = input;
+    var parts = input.split(/@/);
+    this.active = false;
+    if (parts.length === 2) {
+        this.local = parts[0];
+        this.domain = parts[1];
+        this.active = true;
+        this.url = this.domain.replace(/\*\./g, "").replace(/\*/g, "");
+        this.favicon = "http://www.google.com/s2/favicons?domain=" + this.url;
+    }
 };
 
 Rule.fn = Rule.prototype;
 
 Rule.fn.toString = function() {
-	return this.value;
+    return this.value;
 };
 
 Rule.fn.delete = function() {
-	this.active = false;
+    this.active = false;
 };
 
 // Rules
 
 var Rules = function(input) {
-	this.init();
+    this.init();
 };
 
 Rules.fn = Rules.prototype;
 
 Rules.fn.init = function() {
-	this.items = {};
+    this.items = {};
 };
 
 Rules.fn.add = function(rule) {
-	if (typeof rule === "string") {
-		rule = new Rule(rule);
-	}
-	this.items[rule.value] = rule;
-	return this;
+    if (typeof rule === "string") {
+        rule = new Rule(rule);
+    }
+    this.items[rule.value] = rule;
+    return this;
 }
 
 Rules.fn.parseInputString = function(input) {
-	this.init();
-	input.split(/\|/g).forEach(this.add.bind(this));
+    this.init();
+    input.split(/\|/g).forEach(this.add.bind(this));
 };
 
 Rules.fn.toArray = function() {
-	var items = [];
-	for (key in this.items) {
-		items.push(this.items[key]);
-	}
-	return items;
+    var items = [];
+    for (key in this.items) {
+        items.push(this.items[key]);
+    }
+    return items;
 };
 
 Rules.fn.toAlphabetizedArray = function() {
-	var clean = function(input) {
-		return input.replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
-	}
-	return this.toArray().sort(function(a,b) {
-		if (a.domain && b.domain) {
-			return clean(a.domain) >= clean(b.domain) ? 1 : -1;
-		}
-		return 0;
-	})
+    var clean = function(input) {
+        return input.replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
+    }
+    return this.toArray().sort(function(a,b) {
+        if (a.domain && b.domain) {
+            return clean(a.domain) >= clean(b.domain) ? 1 : -1;
+        }
+        return 0;
+    })
 };
 
 Rules.fn.toString = function() {
-	return this.toArray().filter(function(item) {
-		return item.active;
-	}).join("|");
+    return this.toArray().filter(function(item) {
+        return item.active;
+    }).join("|");
 };
 
 // RuleView
 
 var RuleView = function(options) {
-	this.model = options.model;
-	this.el = options.el || document.createElement("tr");
+    this.model = options.model;
+    this.el = options.el || document.createElement("tr");
 };
 
 RuleView.fn = RuleView.prototype;
 
 RuleView.fn.render = function() {
-	var model = this.model;
-	this.el.innerHTML =
-		"<tr data-value='" + model.value + "'>" +
-			"<td class='favicon'><img src='" + model.favicon + "' /></td>" +
-			"<td class='domain'><a href='http://" + model.url + "/' target='_blank'>" + model.domain + "</a></td>" +
-			"<td class='local'>" + model.local + "</td>" +
-			"<td class='delete'>&times;</td>" +
-		"</tr>"
-	this.el.querySelector(".delete").addEventListener("click", function() {
-		this.el.remove();
-		model.delete();
-	}.bind(this));
-	return this;
+    var model = this.model;
+    this.el.innerHTML =
+        "<tr data-value='" + model.value + "'>" +
+            "<td class='favicon'><img src='" + model.favicon + "' /></td>" +
+            "<td class='domain'><a href='http://" + model.url + "/' target='_blank'>" + model.domain + "</a></td>" +
+            "<td class='local'>" + model.local + "</td>" +
+            "<td class='delete'>&times;</td>" +
+        "</tr>"
+    this.el.querySelector(".delete").addEventListener("click", function() {
+        this.el.remove();
+        model.delete();
+    }.bind(this));
+    return this;
 };
--- views.js
+++ views.js
@@ -1,74 +1,74 @@
 // RulesView
 
 var RulesView = function(options) {
-	this.el = options.el;
-	this.model = options.model;
+    this.el = options.el;
+    this.model = options.model;
 };
 
 RulesView.fn = RulesView.prototype;
 
 RulesView.fn.render = function() {
-	this.el.innerHTML = "";
-	this.model.toAlphabetizedArray().forEach(function(model) {
-		if (model.active) {
-			var view = new RuleView({
-				model: model
-			})
-			this.el.appendChild(view.render().el);
-		}
-	}.bind(this));
+    this.el.innerHTML = "";
+    this.model.toAlphabetizedArray().forEach(function(model) {
+        if (model.active) {
+            var view = new RuleView({
+                model: model
+            })
+            this.el.appendChild(view.render().el);
+        }
+    }.bind(this));
 }
 
 // SectionView
 
 var SectionView = function(options) {
-	this.el = options.el;
-	this.sectionNext = undefined;
-	this.onShow = options.onShow || function(){};
-	this.onHide = options.onHide || function(){};
+    this.el = options.el;
+    this.sectionNext = undefined;
+    this.onShow = options.onShow || function(){};
+    this.onHide = options.onHide || function(){};
 };
 
 SectionView.fn = SectionView.prototype;
 
 SectionView.fn.render = function() {
-	this.el.addEventListener("click", function(event) {
-		if (event.target.dataset.nav) {
-			event.preventDefault();
-		}
-	});
-	var navNext = this.el.querySelector("[data-nav='next']");
-	if (navNext) {
-		navNext.addEventListener("click", this.showNext.bind(this));
-	}
-	var navBack = this.el.querySelector("[data-nav='back']");
-	if (navBack) {
-		navBack.addEventListener("click", this.showBack.bind(this));
-	}
-	return this;
+    this.el.addEventListener("click", function(event) {
+        if (event.target.dataset.nav) {
+            event.preventDefault();
+        }
+    });
+    var navNext = this.el.querySelector("[data-nav='next']");
+    if (navNext) {
+        navNext.addEventListener("click", this.showNext.bind(this));
+    }
+    var navBack = this.el.querySelector("[data-nav='back']");
+    if (navBack) {
+        navBack.addEventListener("click", this.showBack.bind(this));
+    }
+    return this;
 };
 
 SectionView.fn.show = function() {
-	this.el.style.display = "block";
-	this.onShow.bind(this)();
-	return this;
+    this.el.style.display = "block";
+    this.onShow.bind(this)();
+    return this;
 };
 
 SectionView.fn.hide = function() {
-	this.el.style.display = "none";
-	this.onHide.bind(this)();
-	return this;
+    this.el.style.display = "none";
+    this.onHide.bind(this)();
+    return this;
 };
 
 SectionView.fn.showNext = function() {
-	if (this.sectionNext) {
-		this.hide();
-		this.sectionNext.show();
-	}
+    if (this.sectionNext) {
+        this.hide();
+        this.sectionNext.show();
+    }
 };
 
 SectionView.fn.showBack = function() {
-	if (this.sectionBack) {
-		this.hide();
-		this.sectionBack.show();
-	}
+    if (this.sectionBack) {
+        this.hide();
+        this.sectionBack.show();
+    }
 };
